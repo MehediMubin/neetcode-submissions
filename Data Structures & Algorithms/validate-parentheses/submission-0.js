2e0636db729b55cf865cb140f29cd09af0ba20b3
Md. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2026-07-20 16:02:58 +0600

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stk = [];
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+                stk.push(s[i]);
+            } else {
+                if (s[i] === ")" && stk[stk.length - 1] === "(") {
+                    stk.pop();
+                } else if (s[i] === "}" && stk[stk.length - 1] === "{") {
+                    stk.pop();
+                } else if (s[i] === "]" && stk[stk.length - 1] === "[") {
+                    stk.pop();
+                } else {
+                    stk.push(s[i]);
+                }
+            }
+        }
+
+        if (stk.length === 0) return true;
+        return false;
+    }
+}
